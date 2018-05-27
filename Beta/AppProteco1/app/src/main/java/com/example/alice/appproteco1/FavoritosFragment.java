a20e7a412170a140abce3f68969a950e3b159f51
Néstor Iván <Nets@MacBook-Pro-de-Maria.local>
2018-05-26 21:10:56 -0500

    listo para david

--- Beta/AppProteco1/app/src/main/java/com/example/alice/appproteco1/FavoritosFragment.java
+++ Beta/AppProteco1/app/src/main/java/com/example/alice/appproteco1/FavoritosFragment.java
@@ -26,59 +26,6 @@ import static android.app.Activity.RESULT_OK;
 
 public class FavoritosFragment extends Fragment {
 
-    private static int SIGN_IN_REQUEST_CODE = 1;
-    private FirebaseListAdapter<ChatMessage> adapter;
-    RelativeLayout activity_main;
-    FloatingActionButton fab;
+    
 
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == SIGN_IN_REQUEST_CODE) {
-            if (requestCode == RESULT_OK){
-                Snackbar.make(activity_main, "Successfuly signed in", Snackbar.LENGTH_SHORT).show();
-                displayChatMessage();
-            }else{
-                Snackbar.make(activity_main, "We couldnt sign you. Try again later", Snackbar.LENGTH_SHORT).show();
-
-            }
-        }
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-        final View view= inflater.inflate(R.layout.fragment_favoritos, container, false);
-        Toast.makeText(getContext(), "Welcome " , Toast.LENGTH_SHORT).show();
-
-
-        activity_main = view.findViewById(R.id.fragment_favoritos);
-        fab = view.findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText input = v.findViewById(R.id.input);
-                FirebaseDatabase.getInstance().getReference().push().setValue(new ChatMessage(input.getText().toString(),
-                        FirebaseAuth.getInstance().getCurrentUser().getEmail()));
-                input.setText("");
-            }
-        });
-
-        Snackbar.make(activity_main, "Welcome //user//", Snackbar.LENGTH_SHORT).show();
-
-        //displayChatMessage();
-
-        return view;
-
-    }
-
-    private void displayChatMessage() {
-
-        ListView listView = getView().findViewById(R.id.list_of_messages);
-    }
 }
