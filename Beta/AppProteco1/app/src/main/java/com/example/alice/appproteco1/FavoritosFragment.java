56a05946b665d4beefff383d7bcada59457fedb4
Néstor Iván <Nets@MacBook-Pro-de-Maria.local>
2018-05-27 13:03:50 -0500

    basic login/signout

--- Beta/AppProteco1/app/src/main/java/com/example/alice/appproteco1/FavoritosFragment.java
+++ Beta/AppProteco1/app/src/main/java/com/example/alice/appproteco1/FavoritosFragment.java
@@ -18,9 +18,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -41,6 +43,53 @@ import static android.app.Activity.RESULT_OK;
 
 public class FavoritosFragment extends Fragment {
 
+    private Button signOutBtn;
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        View view= inflater.inflate(R.layout.fragment_favoritos, container, false);
+
+        signOutBtn = view.findViewById(R.id.signOutButton);
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(Usuario.iniciadoSesion()){
+                                 Toast.makeText(getContext(), "Ha iniciado sesion", Toast.LENGTH_SHORT).show();
+                                 signOutBtn.setText("Sign out");
+
+
+                                 signOutBtn.setOnClickListener(new View.OnClickListener() {
+                                     @Override
+                                     public void onClick(View v) {
+                                         Toast.makeText(getContext(), "Cerrando sesion", Toast.LENGTH_SHORT).show();
+                                         Usuario.cerrarSesion();
+                                     }
+
+                                 });
+
+
+        }else {
+            Toast.makeText(getContext(), "No ha iniciado sesion", Toast.LENGTH_SHORT).show();
+            signOutBtn.setText("Log in");
+
+            signOutBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getContext(), "Iniciar Sesión", Toast.LENGTH_SHORT).show();
+                    Intent tx = new Intent(getActivity(), Ingreso.class);
+                    startActivity(tx);
+                }
+
+            });
+
+        }
+    }
 }
 
