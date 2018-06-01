7f701c0dff441fa3c50b48a7207ca237f77ae3cc
AlicePeach <alicepeachmelocoton2@gmail.com>
2018-06-01 09:26:08 -0500

    Unete

--- Beta/AppProteco1/app/src/main/java/com/example/alice/appproteco1/SettingsFragment.java
+++ Beta/AppProteco1/app/src/main/java/com/example/alice/appproteco1/SettingsFragment.java
@@ -1,5 +1,7 @@
 package com.example.alice.appproteco1;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +9,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class SettingsFragment extends Fragment{
 
@@ -14,9 +20,16 @@ public class SettingsFragment extends Fragment{
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         View view= inflater.inflate(R.layout.fragment_settings, container, false);
+       /* Button btn = (Button)view.findViewById(R.id.btnUbicacion);
 
-        return view;
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Bmeters = new Intent( getActivity(), MapsActivity.class);
+                startActivity(Bmeters);
+            }
+        });
+        return view;*/
     }
 }
